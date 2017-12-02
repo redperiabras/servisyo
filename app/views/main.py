ed7627ec77a288b7fb32a814923402065f1309d8
redperiabras <redperiabras@gmail.com>
2017-12-03 00:40:34 +0800

    Adding lester's picture

--- app/views/main.py
+++ app/views/main.py
@@ -1,11 +1,12 @@
 from flask import render_template, jsonify
+from flask.ext.login import current_user, login_required
 from app import app
 import random
 
 
 @app.route('/')
 def index():
-    return render_template('index.html', title='Home')
+    return render_template('index.html', title='Home', current_user=current_user)
 
 @app.route('/map/refresh', methods=['POST'])
 def map_refresh():
